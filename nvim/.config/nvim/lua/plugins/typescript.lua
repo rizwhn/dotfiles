6340aee789eb291bc4f38fb5d9374f655315f4e2
riz <riz@agilefoxhq.com>
2026-01-11 13:47:50 +0100

    chore: Remove `format_on_save` for prettierd in `typescript.lua`

--- nvim/.config/nvim/lua/plugins/typescript.lua
+++ nvim/.config/nvim/lua/plugins/typescript.lua
@@ -151,10 +151,6 @@ return {
         css = { "prettierd" },
         html = { "prettierd" },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
     },
   },
 
