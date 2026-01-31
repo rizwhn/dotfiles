3c79b4511c8376955507f63a94f045c4183d6089
rizwaniqbal <rizwan.iqbal@grammarly.com>
2026-02-01 00:53:58 +0100

    chore: Update aicommits plugin configuration

--- nvim/.config/nvim/lua/plugins/aicommits.lua
+++ nvim/.config/nvim/lua/plugins/aicommits.lua
@@ -2,21 +2,30 @@ return {
   "404pilo/aicommits.nvim",
   config = function()
     require("aicommits").setup({
-      active_provider = "gemini-api",
+      active_provider = "openai",
       ui = {
         use_custom_picker = true,
         picker = {
-          width = 0.4,             -- Percentage of screen width
-          height = 0.3,            -- Percentage of screen height
-          border = "rounded",      -- Border style
+          width = 0.4, -- Percentage of screen width
+          height = 0.3, -- Percentage of screen height
+          border = "rounded", -- Border style
         },
       },
       providers = {
         ["gemini-api"] = {
-          enabled = true,
+          enabled = false,
           model = "gemini-2.5-flash",
           max_length = 200,
           temperature = 0.8,
+          max_tokens = 500,
+          generate = 3,
+        },
+        ["openai"] = {
+          enabled = true,
+          endpoint = "http://localhost:11434/v1/chat/completions",
+          model = "gemma2:2b",
+          max_length = 200,
+          temperature = 0.7,
           max_tokens = 500,
           generate = 3,
         },
