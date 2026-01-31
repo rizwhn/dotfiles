46cfa7a31c9ea5ba1e534427a964f16c39621b7a
rizwaniqbal <rizwan.iqbal@grammarly.com>
2026-01-31 08:57:11 +0100

    fix(noice): nvim was crashing because of these changes
    - there were errors in the go config that made nvim crash

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -10,69 +10,47 @@ return {
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
-    opts = function(_, opts)
-      opts.servers = opts.servers or {}
-      opts.servers.gopls = {
-        cmd = { vim.fn.stdpath("data") .. "/mason/bin/gopls" },
-        mason = false,
-        autostart = true,
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        root_markers = { "go.work", "go.mod", ".git" },
-        single_file_support = true,
-        settings = {
-          gopls = {
-            gofumpt = true,
-            codelenses = {
-              gc_details = false,
-              generate = true,
-              regenerate_cgo = true,
-              run_govulncheck = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
             },
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
-            analyses = {
-              nilness = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
-            },
-            usePlaceholders = true,
-            completeUnimported = true,
-            staticcheck = true,
-            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-            semanticTokens = true,
           },
         },
-      }
-
-      opts.setup = opts.setup or {}
-      opts.setup.gopls = function(_, server_opts)
-        vim.lsp.config("gopls", server_opts)
-        vim.lsp.enable("gopls")
-        if vim.v.vim_did_enter == 1 then
-          vim.cmd.doautoall("nvim.lsp.enable FileType")
-        else
-          vim.api.nvim_create_autocmd("VimEnter", {
-            once = true,
-            callback = function()
-              vim.cmd.doautoall("nvim.lsp.enable FileType")
-            end,
-          })
-        end
-        return true
-      end
-    end,
+      },
+    },
   },
 
   {
